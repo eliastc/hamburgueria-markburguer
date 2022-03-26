94e8ba0ceff46b3227a8afe06f7ae379f183571c
Elias Calado <eliascalado@hotmail.com>
2022-03-25 21:00:26 -0300

    Project created

---
+++ backend/src/main/java/com/mpinfo/markburguer/MarkburguerApplication.java
@@ -0,0 +1,13 @@
+package com.mpinfo.markburguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkburguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkburguerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mpinfo/markburguer/MarkburguerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpinfo.markburguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkburguerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
